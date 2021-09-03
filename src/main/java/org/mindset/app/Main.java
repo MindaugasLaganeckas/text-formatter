89b3eefc255593fd43bc160c21a9ca331fc77d2d
Mindaugas Laganeckas <Mindaugas.Laganeckas@3shape.com>
2021-09-03 15:47:49 +0200

    Making code testable

--- src/main/java/org/mindset/app/Main.java
+++ src/main/java/org/mindset/app/Main.java
@@ -4,31 +4,44 @@ import org.mindset.inout.DataPrinter;
 import org.mindset.inout.DataReader;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        final DataPrinter printer = new DataPrinter();
-        final InputParser inputParser = new InputParser(printer);
-        final TextFormattingInputs inputs = inputParser.parse(args);
-        final TextAlignmentStrategyFactory factory = new TextAlignmentStrategyFactory();
-        final TextAlignmentStrategy alignment = factory.createAlignmentStrategy(inputs);
+    public static void main(String[] args) throws IOException {
 
         try (final DataReader reader = new DataReader()) {
-            boolean quit = false;
-            printer.println("Enter your text. To quit, type 'q'.");
-            while (!quit && reader.hasNextLine()){
-                final String nextLine = reader.nextLine();
-                if ("q".equals(nextLine)) {
-                    quit = true;
-                } else {
-                    printer.println(alignment.format(nextLine));
-                }
+
+            final DataPrinter printer = new DataPrinter();
+            final InputParser inputParser = new InputParser(printer);
+            final TextAlignmentStrategyFactory factory = new TextAlignmentStrategyFactory();
+
+            final Main app = new Main(reader, printer, factory);
+            app.formatUserInputFromSTDIN(inputParser.parse(args));
+        }
+    }
+
+    private final DataReader reader;
+    private final DataPrinter printer;
+    private final TextAlignmentStrategyFactory factory;
+
+    public Main(final DataReader reader, final DataPrinter printer, final TextAlignmentStrategyFactory factory) {
+        this.reader = reader;
+        this.printer = printer;
+        this.factory = factory;
+    }
+
+    public void formatUserInputFromSTDIN(final TextFormattingInputs inputs) throws IOException {
+
+        final TextAlignmentStrategy alignment = factory.createAlignmentStrategy(inputs);
+        boolean quit = false;
+        printer.println("Enter your text. To quit, type 'q'.");
+        while (!quit && reader.hasNextLine()){
+            final String nextLine = reader.nextLine();
+            if ("q".equals(nextLine)) {
+                quit = true;
+            } else {
+                printer.println(alignment.format(nextLine));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
